d87210738f3f2b66419a6de2f7dbe08ff7bbc05c
Oisin Carroll <carroll.oisin@gmail.com>
2020-04-29 16:04:48 +0100

    initial commit
    initial commit - empty files

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+	define("ROOT", "D:/Projects/PhpBB", true);
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<head>
+	<title>phpbbtest</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	
+	<div class="main">
+		<p>Test Test Test</p>
+	</div>
+
+</body>
+
+<script
+		src="https://code.jquery.com/jquery-3.5.0.min.js"
+		integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="
+		crossorigin="anonymous"></script>
+
+<script type='text/javascript' src='js/main.js'></script>
+
+</html>
